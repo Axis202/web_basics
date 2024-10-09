7d2b02ae35f25886e90090d9dc8db64567d5d6cd
10114245 <Hamidreza.zamani@flender.com>
2024-10-09 12:23:14 +0200

    commit

--- script.js
+++ script.js
@@ -2,16 +2,18 @@
 function generateFact(person) {
   const facts = getFacts(person);
   const randomFact = facts[Math.floor(Math.random() * facts.length)];
-  document.getElementById("random-fact").textContent = randomFact;
+  document.getElementById('random-fact').textContent = randomFact;
 }
 
-// Search functionality
-const searchBar = document.getElementById("search-bar");
-searchBar.addEventListener("input", function () {
-  const query = searchBar.value.toLowerCase();
-  document.querySelectorAll("main p, main h1").forEach(function (element) {
-    element.style.display = element.textContent.toLowerCase().includes(query)
-      ? "block"
-      : "none";
+// Search bar functionality
+const searchBar = document.getElementById('search-bar');
+
+// Check if the search bar exists before adding the event listener
+if (searchBar) {
+  searchBar.addEventListener('input', function () {
+    const query = searchBar.value.toLowerCase();
+    document.querySelectorAll('main p, main h1').forEach(function (element) {
+      element.style.display = element.textContent.toLowerCase().includes(query) ? 'block' : 'none';
+    });
   });
-});
+}
